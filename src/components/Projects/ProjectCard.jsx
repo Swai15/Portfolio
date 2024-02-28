4fcd2a94d4402924c021a250a414bbb02f546a8a
Swai15 <swaisilvano5@gmail.com>
2024-02-28 16:55:19 +0300

    projects section v2

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,28 +4,30 @@ import styles from "./ProjectCard.module.css";
 export const ProjectCard = ({ project: { title, imageSrc, description, skills, demo, source }, theme }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.image}>
-        <img src="/images/placeholder.jpg" alt="Project image" />
+      <div className={styles.imageContainer}>
+        <div className={styles.image}>
+          <img src="/images/placeholder.jpg" alt="Project image" />
+        </div>
       </div>
 
-      <div className={styles.content}>
-        <div className={styles.title}>
-          <h3 className={styles.cursor}>
-            <p>{title}</p> {theme === "light" ? <img className={styles.link} src="./Icons/link-light.png" alt="" /> : <img className={styles.link} src="./Icons/link-dark.png" alt="" />}
-          </h3>
-
-          <a className={styles.source} href="">
-            view source
-          </a>
-        </div>
-
-        <div className={styles.details}>
-          <p className={styles.description}>{description}</p>
-          <ul className={styles.skills}>
-            {skills.map((skill, id) => {
-              return <li className={styles.skill}>{skill}</li>;
-            })}
-          </ul>
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <h3 className={styles.cursor}>
+              <p>{title}</p> {theme === "light" ? <img className={styles.link} src="./Icons/link-light.png" alt="" /> : <img className={styles.link} src="./Icons/link-dark.png" alt="" />}
+            </h3>
+            <a className={styles.source} href="">
+              view source
+            </a>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.description}>{description}</p>
+            <ul className={styles.skills}>
+              {skills.map((skill, id) => {
+                return <li className={styles.skill}>{skill}</li>;
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
